81cfedd16a1c7741bcd412afce549243d350fbdd
ShiromaHajime <hajishiro.hs@gmail.com>
2024-10-17 11:59:14 -0300

    [feat]: Nuevo Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import Colors from '../constants/Colors';
 
 const Header = () => {
   return (
     <View style={styles.header}>
-      <Text style={styles.title}>Mi Aplicación</Text>
+      <Text style={styles.title}>Farmacias cercanas</Text>
     </View>
   );
 };
@@ -12,15 +13,15 @@ const Header = () => {
 const styles = StyleSheet.create({
   header: {
     width: '100%',
-    height: 60,
-    backgroundColor: '#6200EE', // Cambia el color de fondo
+    height: 70,
+    backgroundColor: '#FFFFFF', // Cambia el color de fondo
     justifyContent: 'center',   // Centra el contenido verticalmente
     alignItems: 'center',       // Centra el contenido horizontalmente
     paddingTop: 15,
   },
   title: {
-    color: '#fff',
-    fontSize: 20,
+    color: Colors.primary,
+    fontSize: 23,
     fontWeight: 'bold',
   },
 });
